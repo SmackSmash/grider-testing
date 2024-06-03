c0a762674734e7a7e3a32d149dfcc01f0b109cfc
Dan Moffat <danmoffat@gmail.com>
2024-06-03 19:46:21 +0100

    Built basic HTML for user list component

--- src/components/UserList.tsx
+++ src/components/UserList.tsx
@@ -1,10 +1,15 @@
 const UserList = () => {
   return (
-    <ul>
-      <li>User 1</li>
-      <li>User 2</li>
-      <li>User 3</li>
-    </ul>
+    <div className='mt-4 border-t pt-2'>
+      <h1 className='text-poimandres-black mb-2 text-2xl font-bold'>List of Users</h1>
+      <table>
+        <thead>
+          <td>Name</td>
+          <td>Email</td>
+        </thead>
+        <tbody></tbody>
+      </table>
+    </div>
   );
 };
 
