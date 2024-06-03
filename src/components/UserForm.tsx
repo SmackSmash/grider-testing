e6ab2bfd4afbc4bc3643a14424206c32eca53532
Dan Moffat <danmoffat@gmail.com>
2024-06-03 19:43:23 +0100

    A little styling

--- src/components/UserForm.tsx
+++ src/components/UserForm.tsx
@@ -1,22 +1,26 @@
 const UserForm = () => {
   return (
     <form className='width-full'>
-      <h1 className='text-poimandres-black mb-2 text-3xl font-bold'>Add User</h1>
-      <label htmlFor='name'>Name</label>
+      <h1 className='text-poimandres-black mb-2 text-2xl font-bold'>Add User</h1>
+      <label className='mb-1 mt-2 inline-block' htmlFor='name'>
+        Name
+      </label>
       <input
         className='w-full rounded bg-poimandres-darkslate p-2 outline-none'
         type='text'
         name='name'
         id='name'
       />
-      <label htmlFor='email'>Email</label>
+      <label className='mb-1 mt-2 inline-block' htmlFor='email'>
+        Email
+      </label>
       <input
         className='w-full rounded bg-poimandres-darkslate p-2 outline-none'
         type='email'
         name='email'
         id='email'
       />
-      <button className='mt-2 w-full rounded bg-poimandres-lightgreen px-4 py-2 font-bold text-poimandres-blackslate hover:bg-blue-700'>
+      <button className='mt-4 w-full rounded bg-poimandres-midgreen px-4 py-2 font-bold text-poimandres-blackslate hover:relative hover:-top-px hover:bg-poimandres-lightgreen active:top-0'>
         Add
       </button>
     </form>
