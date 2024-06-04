bec8d8891d27cf07410d254f39790a5268449085
Dan Moffat <danmoffat@gmail.com>
2024-06-04 02:34:59 +0100

    Added test to check rendered lines

--- src/components/UserList.test.tsx
+++ src/components/UserList.test.tsx
@@ -2,9 +2,24 @@ import { render, screen } from '@testing-library/react';
 import UserList from './UserList';
 
 describe('UserList', () => {
-  it('should render paragraph when no users in list', () => {
+  it('renders a paragraph when no users in list', () => {
     render(<UserList users={[]} />);
     const paragraph = screen.getByRole('paragraph');
     expect(paragraph).toBeInTheDocument();
+  });
+
+  it('renders one line per user', () => {
+    render(
+      <UserList
+        users={[
+          { id: '1', name: 'Dan', email: 'dan@test.com' },
+          { id: '1', name: 'Steve', email: 'steve@test.com' }
+        ]}
+      />
+    );
+    const danRow = screen.getByRole('row', { name: /dan dan@test\.com/i });
+    const steveRow = screen.getByRole('row', { name: /steve steve@test\.com/i });
+    expect(danRow).toBeInTheDocument();
+    expect(steveRow).toBeInTheDocument();
   });
 });
