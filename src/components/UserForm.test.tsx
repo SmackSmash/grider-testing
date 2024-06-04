01fec534898daeb2ab536a22b36c475ea2cb65ce
Dan Moffat <danmoffat@gmail.com>
2024-06-04 01:21:07 +0100

    Added test for form submission

--- src/components/UserForm.test.tsx
+++ src/components/UserForm.test.tsx
@@ -1,6 +1,7 @@
 import { render, screen } from '@testing-library/react';
-// import user from '@testing-library/user-event';
+import user from '@testing-library/user-event';
 import UserForm from './UserForm';
+import { type User } from '../types';
 
 describe('UserForm', () => {
   it('has a header', () => {
@@ -15,5 +16,19 @@ describe('UserForm', () => {
     const button = screen.getByRole('button');
     expect(inputs).toHaveLength(2);
     expect(button).toBeInTheDocument();
+  });
+
+  it('calls addUser when the form is submitted', async () => {
+    const argList: User[] = [];
+    const callback = (user: User) => {
+      argList.push(user);
+    };
+    render(<UserForm addUser={callback} />);
+    const [name, email] = screen.getAllByRole('textbox');
+    await user.type(name, 'Test name');
+    await user.type(email, 'test@email.com');
+    const button = screen.getByRole('button');
+    await user.click(button);
+    expect(argList).toHaveLength(1);
   });
 });
